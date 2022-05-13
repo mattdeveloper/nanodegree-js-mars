e61d2ff6a9e078b064f68951ebc5cd8bcb0c4e4a
Matt Oliveira <talk@mattdeveloper.com>
2022-05-13 07:39:40 -0300

    add immutable.js

--- src/public/client.js
+++ src/public/client.js
@@ -1,8 +1,10 @@
-let store = {
+const store = Immutable.Map({
   user: { name: 'Student' },
   apod: '',
   rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-};
+});
+
+console.log(store.toObject());
 
 // add our markup to the page
 const root = document.getElementById('root');
@@ -18,15 +20,28 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-  let { rovers, apod } = state;
+  console.log(state);
+  const { rovers, apod, user } = state;
 
   return `
     <header></header>
     <main>
-      ${Greeting(store.user.name)}
-      <section>
-        <h3>Put things on the page!</h3>
-        <p>Here is an example section.</p>
+      <section class="container mb-4">
+        ${Greeting(user.name)}
+      </section>
+
+      ${
+        rovers &&
+        `
+        <section class="container">
+          <div class="row">
+            ${rovers.map((rover) => RoverComponent(rover)).join('')}
+          </div>
+        </section>
+        `
+      }
+      
+      <section class="container mt-5">
         <p>
           One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
           the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
@@ -44,7 +59,7 @@ const App = (state) => {
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-  render(root, store);
+  render(root, store.toObject());
 });
 
 // ------------------------------------------------------  COMPONENTS
@@ -52,10 +67,18 @@ window.addEventListener('load', () => {
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 const Greeting = (name) => {
   if (name) {
-    return `<h1>Welcome, ${name}!</h1>`;
+    return `<h1 class="border-bottom">Welcome, ${name}!</h1>`;
   }
 
-  return `<h1>Hello!</h1>`;
+  return `<h1 class="border-bottom">Hello!</h1>`;
+};
+
+const RoverComponent = (rover) => {
+  return `
+    <section class="col-6 col-md-4 col-lg-3">
+      <h3>Rover ${rover}!</h3>
+    </section>
+  `;
 };
 
 // Example of a pure function that renders infomation requested from the backend
@@ -63,11 +86,9 @@ const ImageOfTheDay = (apod) => {
   // If image does not already exist, or it is not from today -- request it again
   const today = new Date();
   const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
 
-  console.log(photodate.getDate() === today.getDate());
   if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
+    getImageOfTheDay(store.toObject());
   }
 
   // check if the photo of the day is actually type video!
@@ -79,7 +100,7 @@ const ImageOfTheDay = (apod) => {
     `;
   } else {
     return `
-      <img src="${apod.image.url}" height="350px" width="100%" />
+      <img src="${apod.image.url}" width="100%" />
       <p>${apod.image.explanation}</p>
     `;
   }
@@ -93,7 +114,7 @@ const getImageOfTheDay = (state) => {
 
   fetch(`http://localhost:3000/apod`)
     .then((res) => res.json())
-    .then((apod) => updateStore(store, { apod }));
+    .then((apod) => updateStore(store.toObject(), { apod }));
 
   return data;
 };
