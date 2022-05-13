6dc7e428333f3e0b8c4054005d6b8e1dfaa89e77
Matt Oliveira <talk@mattdeveloper.com>
2022-05-13 07:01:58 -0300

    config prettier and eslint

--- src/public/client.js
+++ src/public/client.js
@@ -1,11 +1,11 @@
 let store = {
-  user: { name: "Student" },
-  apod: "",
-  rovers: ["Curiosity", "Opportunity", "Spirit"],
+  user: { name: 'Student' },
+  apod: '',
+  rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 };
 
 // add our markup to the page
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
   store = Object.assign(store, newState);
@@ -21,29 +21,29 @@ const App = (state) => {
   let { rovers, apod } = state;
 
   return `
-      <header></header>
-      <main>
-        ${Greeting(store.user.name)}
-        <section>
-          <h3>Put things on the page!</h3>
-          <p>Here is an example section.</p>
-          <p>
-            One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-            the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-            This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-            applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-            explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-            but generally help with discoverability of relevant imagery.
-          </p>
-          ${ImageOfTheDay(apod)}
-        </section>
-      </main>
-      <footer></footer>
-    `;
+    <header></header>
+    <main>
+      ${Greeting(store.user.name)}
+      <section>
+        <h3>Put things on the page!</h3>
+        <p>Here is an example section.</p>
+        <p>
+          One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+          the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+          This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+          applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+          explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+          but generally help with discoverability of relevant imagery.
+        </p>
+        ${ImageOfTheDay(apod)}
+      </section>
+    </main>
+    <footer></footer>
+  `;
 };
 
 // listening for load event because page should load before any JS is called
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   render(root, store);
 });
 
@@ -71,7 +71,7 @@ const ImageOfTheDay = (apod) => {
   }
 
   // check if the photo of the day is actually type video!
-  if (apod.media_type === "video") {
+  if (apod.media_type === 'video') {
     return `
       <p>See today's featured video <a href="${apod.url}">here</a></p>
       <p>${apod.title}</p>
